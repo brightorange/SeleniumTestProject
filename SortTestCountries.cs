3cd6d0b6aaa35908384a3f65515209e4d6e3557e
inga <inga-loputneva@yandex.ru>
2016-12-11 21:46:40 +0200

    Sorting Tests

---
+++ SortTestCountries.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace SeleniumTestProject
+{
+    [TestFixture]
+    public class SortTestCountries
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new InternetExplorerDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void TestSortCountries()
+
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
+            var Countries = driver.FindElements(By.CssSelector("td:not([style ^= text-align]) > a"));
+            var countriesText = new List<string>();
+            var sortedCountriesText = new List<string>();
+            foreach (IWebElement countrie in Countries)
+            {
+                countriesText.Add(countrie.Text);
+                sortedCountriesText.Add(countrie.Text);
+            }
+            sortedCountriesText.Sort();
+            Assert.AreEqual(countriesText, sortedCountriesText);
+
+            Countries = driver.FindElements(By.CssSelector(".row"));
+            for (int i = 0; i < Countries.Count; i++)
+            {
+                if (Int32.Parse(Countries[i].FindElement(By.XPath("td[6]")).Text) > 0)
+                {
+                    Countries[i].FindElement(By.XPath("td[7]")).Click();
+                    var Table = driver.FindElement(By.Id("table-zones"));
+                    var Zones = Table.FindElements(By.CssSelector("[name *= zone]"));
+                    var ZonesText = new List<string>();
+                    var sortedZonesText = new List<string>();
+                    foreach (IWebElement zone in Zones)
+                    {
+                        ZonesText.Add(zone.Text);
+                        sortedZonesText.Add(zone.Text);
+                    }
+                    sortedZonesText.Sort();
+                    Assert.AreEqual(ZonesText, sortedZonesText);
+
+                    driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
+                    Countries = driver.FindElements(By.CssSelector(".row"));
+                }
+
+
+                
+            }
+        }
+    
+
+
+
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ SortTestGeoZones.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace SeleniumTestProject
+{
+    [TestFixture]
+    public class SortTestGeoZones
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new InternetExplorerDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void TestSortGeoZones()
+
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            driver.Url = "http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones";
+            var GeoZones = driver.FindElements(By.CssSelector(".fa-pencil"));
+            for (int j = 0; j < GeoZones.Count; j++)
+            {
+               GeoZones[j].Click();
+               var GeoZoneCountries = driver.FindElements(By.CssSelector("[name *= zone_code]"));
+               var GeoZoneCountriesText = new List<String>();
+               var SortedGeoZoneCountriesText = new List<String>();
+               for (int k = 0; k < GeoZoneCountries.Count; k++)
+               {
+                  GeoZoneCountriesText.Add(GeoZoneCountries[k].FindElement(By.CssSelector("[selected]")).Text);
+                  SortedGeoZoneCountriesText.Add(GeoZoneCountries[k].FindElement(By.CssSelector("[selected]")).Text);
+               }
+               SortedGeoZoneCountriesText.Sort();
+               Assert.AreEqual(GeoZoneCountriesText, SortedGeoZoneCountriesText);
+               driver.Url = "http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones";
+               GeoZones = driver.FindElements(By.CssSelector(".fa-pencil"));
+            }
+
+            
+        }
+    
+
+
+
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
